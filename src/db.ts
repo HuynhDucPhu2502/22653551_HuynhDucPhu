abacd5fc44e8a7991892a655815b9cf9d8d5ee60
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-11-15 09:51:47 +0700

    feat(Q2): create grocery_items table and seed sample

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,7 @@
 import { SQLiteDatabase } from "expo-sqlite";
 
 export const initTable = async (db: SQLiteDatabase) => {
+  // Tạo bảng nếu chưa có
   await db.execAsync(`
     CREATE TABLE IF NOT EXISTS grocery_items (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -11,4 +12,15 @@ export const initTable = async (db: SQLiteDatabase) => {
       created_at INTEGER
     )
   `);
+
+  const result = await db.getAllSync("SELECT * FROM grocery_items");
+
+  if (result.length === 0) {
+    await db.execAsync(`
+      INSERT INTO grocery_items (name, quantity, bought, created_at) VALUES
+      ('Sữa', 1, 0, ${Date.now()}),
+      ('Trứng', 1, 0, ${Date.now()}),
+      ('Bánh mì', 1, 0, ${Date.now()})
+    `);
+  }
 };
